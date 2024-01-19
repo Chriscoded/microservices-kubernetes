ed4cd4b83af6accbae06c274ac0ca532a9c2b21f
chriscoded <amaemechris@gmail.com>
2024-01-19 15:25:23 +0100

    grpc syncronous

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -1,8 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using PlatformService.AsyncDataServices;
 using PlatformService.Data;
+using PlatformService.SyncDataServices.Grpc;
 using PlatformService.SyncDataServices.Http;
-
+using System.IO;
 var builder = WebApplication.CreateBuilder(args);
 
 // // Add services to the container.
@@ -25,9 +26,11 @@ if (builder.Environment.IsProduction())
 }
 
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
-builder.Services.AddSingleton<IMessageBusClient, MessageBusClient>();
 
 builder.Services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
+builder.Services.AddSingleton<IMessageBusClient, MessageBusClient>();
+builder.Services.AddGrpc();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -56,5 +59,9 @@ app.UseAuthorization();
 PrepDb.PrepPopulation(app, true);
 
 app.MapControllers();
+app.MapGrpcService<GrpcPlatformService>();
+app.MapGet("/protos/platforms.proto", async context => {
+    await context.Response.WriteAsync(File.ReadAllText("Protos/platforms.proto"));
+});
 
 app.Run();
